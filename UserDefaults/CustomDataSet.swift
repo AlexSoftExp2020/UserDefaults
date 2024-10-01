5ca0614ba4c3b5b54159e85b8d577100aa875e43
Alex <oleksii.pidhornyi@gmail.com>
2024-10-01 09:52:46 +0200

    custom data set

--- UserDefaults/CustomDataSet.swift
+++ UserDefaults/CustomDataSet.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    let login: String
+    let lastActivityDate: Date
+}
+
+// save user to UserDefaults
+func save(user: User) {
+    do {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(user)
+        UserDefaults.standard.set(data, forKey: "user")
+    } catch {
+        // handle error
+    }
+}
+// get user from UserDefaults
+func getUser() -> User? {
+    guard let data = UserDefaults.standard.data(forKey: "user") else {
+        return nil
+    }
+    let decoder = JSONDecoder()
+    let user = try? decoder.decode(User.self, from: data)
+    return user
+}
